99a8f8d4d388d7e8d120fb0b89840bc655ad1380
JoleneKearse <pharaohnutz@gmail.com>
2024-05-30 11:02:22 -0400

    chore: update New Note title

--- src/components/NewNote.tsx
+++ src/components/NewNote.tsx
@@ -9,7 +9,7 @@ type NewNoteProps = {
 export function NewNote({ onSubmit, onAddTag, availableTags }: NewNoteProps) {
   return (
     <>
-      <h1 className="my-4 text-3xl font-bold">New Note</h1>
+      <h1 className="my-4 text-3xl font-bold text-transparent bg-gradient-to-br from-violet-600 via-violet-400 to-violet-300 bg-clip-text">New Note</h1>
       <NoteForm onSubmit={onSubmit} onAddTag={onAddTag} availableTags={availableTags} />
     </>
   )
